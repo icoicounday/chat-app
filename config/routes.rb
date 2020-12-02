e52cd1e202be04e1aad545c1e84463135ec1b114
icoicounday <75007893+icoicounday@users.noreply.github.com>
2020-12-02 12:03:18 +0900

    deviseに関するビューファイルの配置

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
+# Rails.application.routes.draw do
+#   get 'messages/index'
+#   root to: "messages#index"
+# end
+
 Rails.application.routes.draw do
-  devise_for :users
-  get 'messages/index'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # ↑↑上の記述は削除しましょう↑↑
   root to: "messages#index"
+  devise_for :users
 end
