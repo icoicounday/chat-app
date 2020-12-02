a801ac9f2dce4a8e3ada935945a07e7ee2d023c5
icoicounday <75007893+icoicounday@users.noreply.github.com>
2020-12-02 20:20:20 +0900

    画面遷移の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,6 @@
-# Rails.application.routes.draw do
-#   get 'messages/index'
-#   root to: "messages#index"
-# end
-
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # ↑↑上の記述は削除しましょう↑↑
-  root to: "messages#index"
   devise_for :users
+  root to: "rooms#index"
   resources :users, only: [:edit, :update]
-  resources :rooms, onry: [:new, :create]
+  resources :rooms, only: [:new, :create]
 end
